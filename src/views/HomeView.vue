312a767bff3f9699b7991bd49f83ee1f0a869c44
c711cat <c711cat@gmail.com>
2024-10-24 14:06:13 +0800

    製作搜尋功能：從資料庫中搜尋，其搜尋的字眼符合樣品名稱或俗名，則顯示於畫面中

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,41 @@
 
 <script>
 import { useFoodStore } from '@/stores/foodDataStore.js'
-
+import { mapActions, mapState } from 'pinia'
 export default {
   data() {
     return {
       foodData: [],
     }
   },
+  methods: {
+    ...mapActions(useFoodStore, ['searchFood']),
+    getFoodData() {
+      const foodStore = useFoodStore()
+      foodStore.fetchFoods().then(() => {
+        this.foodData = foodStore.updateKeyData
+      })
+    },
+  },
+  computed: {
+    ...mapState(useFoodStore, ['updateKeyData']),
+  },
   created() {
-    const foodStore = useFoodStore()
-    foodStore.fetchFoods().then(() => {
-      this.foodData = foodStore.updateKeyData
-    })
+    this.getFoodData()
   },
 }
 </script>
+
+<style lang="scss" scoped>
+* {
+  // border: 1px solid;
+}
+
+ul.list-group {
+  // --bs-list-group-border-width: none;
+}
+
+.foodDataContainer {
+  height: 600px;
+}
+</style>
