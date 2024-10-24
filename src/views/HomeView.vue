3cff4708896aa65898ff1757bdf17534a2bc4956
c711cat <c711cat@gmail.com>
2024-10-24 23:19:57 +0800

    從資料庫選擇成分到右邊成分欄位，並可以填入其成分顯示於外包裝名稱及總食材克數

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,11 +12,12 @@
       <label for="searchInput">請輸入食物成分搜尋</label>
     </div>
     <div class="col-12 justify-content-between row m-0">
-      <div class="col-4 col-xl-2 ps-0 foodDataContainer">
-        <div class="list-group rounded foodDataContainer overflow-y-auto">
+      <div class="col-4 col-xl-2 ps-0">
+        <div class="list-group rounded visibleHeight overflow-y-auto">
           <button
             v-for="item in updateKeyData"
             :key="item.id"
+            @click="chooseSample(item)"
             type="button"
             class="list-group-item list-group-item-action"
             aria-current="true"
@@ -29,12 +30,13 @@
         </div>
       </div>
 
-      <div class="p-0 col-8 col-xl-10">
+      <div class="py-0 pe-0 col-8 col-xl-10">
         <div class="mb-3 col-12">
           <label for="productName" class="form-label fw-bold ps-1">
             商品名稱
           </label>
           <input
+            v-model="product.title"
             type="text"
             class="form-control"
             id="productName"
@@ -45,53 +47,72 @@
         <p class="mb-1 pb-1 ps-1 fw-bold">
           從左邊資料庫點選成分，並填入各欄位所需資料
         </p>
-        <ul
-          class="border-top rounded-0 position-relative col-12 list-group list-group-horizontal-xl"
-        >
-          <li
-            class="list-group-item col-12 col-xl-3 d-flex flex-column justify-content-center"
+        <div class="ingredientsVisibleHeight overflow-y-auto">
+          <ul
+            v-for="(item, index) in product.ingredients"
+            :key="item.id"
+            class="border-top rounded-0 position-relative col-12 list-group list-group-horizontal-xl"
           >
-            <span class="pe-3">樣品名j樣品名c樣品名樣品名樣品名</span>
-            <span>俗名</span>
-          </li>
-          <li
-            class="list-group-item col-12 col-xl-4 d-flex flex-column justify-content-center"
-          >
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="輸入欲呈現於外包裝成分"
-              class="form-control"
-            />
-          </li>
-          <li
-            class="list-group-item col-12 col-xl-4 d-flex flex-column justify-content-center"
-          >
-            <input
-              type="number"
-              name=""
-              id=""
-              min="1"
-              placeholder="輸入該成分的總克數"
-              class="form-control"
-            />
-          </li>
-          <li
-            class="d-none d-xl-block list-group-item col-1 align-content-center"
-          >
-            <button type="button" class="btn btn-outline-danger btn-sm d-block">
-              <i class="bi bi-x-lg"></i>
+            <li
+              class="list-group-item col-12 col-xl-3 d-flex flex-column justify-content-center"
+            >
+              <span class="pe-3">
+                {{ index + 1 }}. {{ item.sample_name }}
+              </span>
+              <span v-if="item.common_name">俗名:{{ item.common_name }}</span>
+            </li>
+            <li
+              class="list-group-item col-12 col-xl-4 d-flex flex-column justify-content-center"
+            >
+              <input
+                v-model="item.foodName"
+                type="text"
+                name=""
+                id=""
+                placeholder="輸入欲呈現於外包裝成分"
+                class="form-control"
+              />
+            </li>
+            <li
+              class="list-group-item col-12 col-xl-4 d-flex flex-column justify-content-center"
+            >
+              <input
+                v-model="item.grams"
+                type="number"
+                name=""
+                id=""
+                min="1"
+                placeholder="輸入該成分的總克數"
+                class="form-control"
+              />
+            </li>
+            <li
+              class="d-none d-xl-block list-group-item col-1 align-content-center"
+            >
+              <button
+                type="button"
+                class="btn btn-outline-danger btn-sm d-block"
+              >
+                <i class="bi bi-x-lg"></i>
+              </button>
+            </li>
+            <li
+              class="d-block d-xl-none position-absolute delBtn-xs list-group"
+            >
+              <button type="button" class="btn btn-outline-danger btn-sm">
+                <i class="bi bi-x-lg"></i>
+              </button>
+            </li>
+          </ul>
+          <div class="my-3 col-12">
+            <button
+              type="button"
+              class="btn btn-primary w-100"
+              @click="addNewField"
+            >
+              新增欄位
             </button>
-          </li>
-          <li class="d-block d-xl-none position-absolute delBtn-xs list-group">
-            <button type="button" class="btn btn-outline-danger btn-sm">
-              <i class="bi bi-x-lg"></i>
-            </button>
-          </li>
-        </ul>
-        <div class="my-3 col-12">
-          <button type="button" class="btn btn-primary w-100">新增欄位</button>
+          </div>
         </div>
       </div>
     </div>
@@ -105,6 +126,18 @@ export default {
   data() {
     return {
       foodData: [],
+      product: {
+        id: '',
+        title: '',
+        ingredients: [
+          {
+            id: '',
+            sample_name: '',
+            foodName: '',
+            grams: '',
+          },
+        ],
+      },
     }
   },
   methods: {
@@ -114,6 +147,31 @@ export default {
       foodStore.fetchFoods().then(() => {
         this.foodData = foodStore.updateKeyData
       })
+    },
+    chooseSample(chooseItem) {
+      console.log(chooseItem)
+      const empty = this.product.ingredients.find(item => {
+        return item.sample_name === ''
+      })
+      if (empty) {
+        empty.id = Date.now()
+        empty.sample_name = chooseItem.sample_name
+        if (chooseItem.common_name) {
+          empty.common_name = chooseItem.common_name
+        }
+      }
+
+      console.log(this.product)
+    },
+    addNewField() {
+      const newIngredient = {
+        id: '',
+        sample_name: '',
+        foodName: '',
+        grams: '',
+      }
+      this.product.ingredients.push(newIngredient)
+      console.log(this.product)
     },
   },
   computed: {
@@ -134,8 +192,12 @@ ul.list-group {
   --bs-list-group-border-width: none;
 }
 
-.foodDataContainer {
-  height: 600px;
+.visibleHeight {
+  height: 70vh;
+}
+
+.ingredientsVisibleHeight {
+  height: 58vh;
 }
 
 .delBtn-xs {
