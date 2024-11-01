1624c0af78e0b1a2bc16c1b72aa96a5b561f6f73
c711cat <c711cat@gmail.com>
2024-11-01 13:24:17 +0800

    微調版面

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -122,15 +122,15 @@
             </tr>
             <tr class="lh-1">
               <th class="fw-normal ps-2">熱量</th>
-              <td class="text-end pe-2">{{ calculateCalories(item) }}大卡</td>
+              <td class="text-end pe-2">{{ calculateCalories(item) }} 大卡</td>
               <td class="text-end pe-2">
-                {{ calculatePer100gCalories(item) }}大卡
+                {{ calculatePer100gCalories(item) }} 大卡
               </td>
             </tr>
             <tr class="lh-1">
               <th class="fw-normal ps-2">蛋白質</th>
               <td class="text-end pe-2">
-                {{ calculateNutrients(item, 'protein') }}公克
+                {{ calculateNutrients(item, 'protein') }} 公克
               </td>
               <td class="text-end pe-2">
                 {{ calculatePer100g(item, 'protein') }} 公克
@@ -148,46 +148,46 @@
             <tr class="lh-1">
               <th class="fw-normal ps-4">飽和脂肪</th>
               <td class="text-end pe-2">
-                {{ calculateNutrients(item, 'saturated_fat') }}公克
+                {{ calculateNutrients(item, 'saturated_fat') }} 公克
               </td>
               <td class="text-end pe-2">
-                {{ calculatePer100g(item, 'saturated_fat') }}公克
+                {{ calculatePer100g(item, 'saturated_fat') }} 公克
               </td>
             </tr>
             <tr class="lh-1">
               <th class="fw-normal ps-4">反式脂肪</th>
               <td class="text-end pe-2">
-                {{ calculateNutrients(item, 'trans_fat') }}公克
+                {{ calculateNutrients(item, 'trans_fat') }} 公克
               </td>
               <td class="text-end pe-2">
-                {{ calculatePer100g(item, 'trans_fat') }}公克
+                {{ calculatePer100g(item, 'trans_fat') }} 公克
               </td>
             </tr>
             <tr class="lh-1">
               <th class="fw-normal ps-2">碳水化合物</th>
               <td class="text-end pe-2">
-                {{ calculateNutrients(item, 'total_carbohydrates') }}公克
+                {{ calculateNutrients(item, 'total_carbohydrates') }} 公克
               </td>
               <td class="text-end pe-2">
-                {{ calculatePer100g(item, 'total_carbohydrates') }}公克
+                {{ calculatePer100g(item, 'total_carbohydrates') }} 公克
               </td>
             </tr>
             <tr class="lh-1">
               <th class="fw-normal ps-4">糖</th>
               <td class="text-end pe-2">
-                {{ calculateNutrients(item, 'total_sugar') }}公克
+                {{ calculateNutrients(item, 'total_sugar') }} 公克
               </td>
               <td class="text-end pe-2">
-                {{ calculatePer100g(item, 'total_sugar') }}公克
+                {{ calculatePer100g(item, 'total_sugar') }} 公克
               </td>
             </tr>
             <tr class="lh-1">
               <th class="fw-normal ps-2">鈉</th>
               <td class="text-end pe-2">
-                {{ calculateNutrients(item, 'sodium') }}毫克
+                {{ calculateNutrients(item, 'sodium') }} 毫克
               </td>
               <td class="text-end pe-2">
-                {{ calculatePer100g(item, 'sodium') }}毫克
+                {{ calculatePer100g(item, 'sodium') }} 毫克
               </td>
             </tr>
           </tbody>
