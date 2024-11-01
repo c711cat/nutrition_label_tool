a1d43a00fffb90322d62dea1c679926b1baaf3b6
c711cat <c711cat@gmail.com>
2024-11-01 13:10:29 +0800

    建立新增其他營養素按鈕，按下按鈕即可開啟modal來點選需要增加的營養素

---
+++ src/components/AddNutrientsModal.vue
@@ -0,0 +1,73 @@
+<template>
+  <div ref="nutrientsModal" class="modal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">新增其他營養素</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="list-group">
+            <button
+              v-for="item in headerChineseAndEnglish"
+              :key="item"
+              type="button"
+              class="list-group-item list-group-item-action"
+              aria-current="true"
+            >
+              {{ item }}
+            </button>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            取消
+          </button>
+          <button type="button" class="btn btn-primary">新增</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from 'bootstrap/js/dist/modal'
+import { useFoodStore } from '@/stores/foodDataStore'
+import { mapActions, mapState } from 'pinia'
+
+export default {
+  data() {
+    return {
+      modal: null,
+      nutrients: {},
+    }
+  },
+  computed: {
+    ...mapState(useFoodStore, ['headerChineseAndEnglish']),
+  },
+  methods: {
+    ...mapActions(useFoodStore, ['fetchFoods']),
+    showModal() {
+      this.modal.show()
+    },
+    hideModal() {
+      this.modal.hide()
+    },
+  },
+  created() {
+    this.fetchFoods()
+  },
+  mounted() {
+    this.modal = new Modal(this.$refs.nutrientsModal)
+  },
+}
+</script>
