267f7d25045ae4789feec3ab8ac01e308c24bbd4
c711cat <c711cat@gmail.com>
2024-11-01 13:43:23 +0800

    暫時新增方便前往分頁的連結

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="container my-3">
+    <router-link to="/nutrition_label" class="btn btn-primary me-3">
+      新增營養標示
+    </router-link>
+    <router-link to="/product_list" class="btn btn-primary">
+      標示列表
+    </router-link>
+  </div>
   <RouterView />
 </template>
 
