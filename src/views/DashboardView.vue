4a7480f7d043c46d64b6a5a1e0f82ced1137ea84
c711cat <c711cat@gmail.com>
2024-11-14 23:14:54 +0800

    將MessageComponent放在DashboardView.vue中

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,14 +1,16 @@
 <template>
   <NavbarComponent />
+  <MessageComponent ref="toast" />
   <RouterView />
 </template>
 
 <script>
 import NavbarComponent from '@/components/NavbarComponent.vue'
+import MessageComponent from '@/components/MessagesComponent.vue'
 export default {
   data() {
     return {}
   },
-  components: { NavbarComponent },
+  components: { NavbarComponent, MessageComponent },
 }
 </script>
