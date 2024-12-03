1ced25a7cad75c034fc43e1a209efc1117a7655c
c711cat <c711cat@gmail.com>
2024-12-03 15:15:49 +0800

    先簡易背著熱量計算規定方式，避免使用者因為增減膳食纖維或酒精，造成的熱量不同而產生困惑

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -261,6 +261,33 @@
           </button>
         </div>
       </div>
+      <div>
+        <p>
+          依據
+          <a
+            href="https://www.foodlabel.org.tw/FdaFrontEndApp/Law/Edit?SystemId=f27b8c5e-35c2-4a86-9b8b-d7cbbef409ab&clPublishStatus=undefined"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            包裝食品營養標示應遵行事項
+          </a>
+          第一章 之 第十一條：包裝食品營養標示之熱量計算方式，應依下列規定辦理：
+        </p>
+        <p>(一) 蛋白質之熱量，以每公克四大卡計算。</p>
+        <p>(二) 脂肪之熱量，以每公克九大卡計算。</p>
+        <p>
+          (三)
+          碳水化合物之熱量，以每公克四大卡計算，但加以標示膳食纖維者，其膳食纖維熱量得以每公克二大卡計算。
+        </p>
+        <p>
+          (四)
+          赤藻糖醇之熱量得以零大卡計算，其他糖醇之熱量得以每公克二‧四大卡計算；有機酸之熱量得以每公克三大卡計算；酒精(乙醇)之熱量得以每公克七大卡計算。並應將糖醇含量標示於營養標示格式中，有機酸及酒精(乙醇)含量應於營養標示格式外明顯處註明。
+        </p>
+        <p>
+          (五)
+          每份熱量計算方式，得以每一百公克(或毫升)之熱量換算之，或以每一百公克(或毫升)之蛋白質、脂肪及碳水化合物含量換算為每份含量後，再以第一款至前款計算方式計算每份之熱量。
+        </p>
+      </div>
     </div>
     <AddNutrientsModal ref="addNutrientsModal" />
     <DoubleCheckModal ref="doubleCheckModal" />
