4772adacd9c4bd1cfafda0289bf8521e44ccf564
c711cat <c711cat@gmail.com>
2025-01-10 15:38:19 +0800

    加上搜尋的功能及相對應的placeholder

--- src/components/SearchBarComponent.vue
+++ src/components/SearchBarComponent.vue
@@ -1,12 +1,14 @@
 <template>
   <main
-    class="wrap ms-auto mt-2 me-2 d-flex justify-content-end align-items-center"
+    v-if="showSearch"
+    class="wrap ms-auto mt-3 me-3 d-flex justify-content-end align-items-center"
     :class="bar ? 'openBar' : ''"
   >
     <form :class="bar ? 'show' : ''" class="me-2 p-1 search-form">
       <input
-        class="form-control"
-        placeholder="請輸入關鍵字"
+        @input="search($event)"
+        class="form-control w-100"
+        :placeholder="currentPlaceHolder"
         type="search"
         name="searchBar"
         id="searchBar"
@@ -30,18 +32,65 @@
 </template>
 
 <script>
+import { useCustomizeStore } from '@/stores/customizeStore'
+import { useFoodStore } from '@/stores/foodDataStore'
+import { mapActions } from 'pinia'
 export default {
   data() {
     return {
       bar: false,
     }
   },
+  computed: {
+    showSearch() {
+      if (this.$route.path === '/') {
+        return false
+      }
+      if (this.$route.path === '/nutrition_label') {
+        return false
+      }
+      if (this.$route.path === '/mark_items') {
+        return false
+      } else {
+        return true
+      }
+    },
+    currentPlaceHolder() {
+      if (this.$route.path === '/product_list') {
+        return '產品名稱'
+      }
+      if (this.$route.path === '/customize_list') {
+        return '自定義資料品名'
+      }
+      if (this.$route.path === '/added_customize_nts') {
+        return '中文或英文營養素名稱'
+      } else {
+        return ''
+      }
+    },
+  },
   methods: {
+    ...mapActions(useFoodStore, ['searchProducts']),
+    ...mapActions(useCustomizeStore, ['searchAddedNts', 'searchCustomizeList']),
     showSearchBar() {
       this.bar = true
     },
     closeSearchBar() {
       this.bar = false
+    },
+    search(e) {
+      console.log(e)
+      const text = e.target.value.trim()
+      const currentPath = this.$router.currentRoute._value.fullPath
+      if (currentPath === '/added_customize_nts') {
+        this.searchAddedNts(text)
+      }
+      if (currentPath === '/customize_list') {
+        this.searchCustomizeList(text)
+      }
+      if (currentPath === '/product_list') {
+        this.searchProducts(text)
+      }
     },
   },
 }
@@ -61,7 +110,7 @@ export default {
 }
 
 .search-form.show {
-  width: 200px;
+  width: 220px;
   opacity: 1;
 }
 
